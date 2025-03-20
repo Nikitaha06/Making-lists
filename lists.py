b3a910755507398d14ddbca6ba973b6df59e676a
Nikitaha06 <nikitanaik2206@gmail.com>
2025-03-20 11:24:26 +0530

    Add files via upload

---
+++ lists.py
@@ -0,0 +1,8 @@
+# Write your code here :-)
+mybreakfast = ["coffee", "chocolate", "bread"]
+
+mybreakfast.append('pills')
+
+print(len(mybreakfast)) #total number of elements in the list, mybreakfast
+print(mybreakfast[3])
+print(mybreakfast)
